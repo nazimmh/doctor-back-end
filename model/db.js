4fb5fde30225ca01e854c55e5908a010e417589b
Moumene <wifi.moumen@gmail.com>
2018-04-28 15:10:37 +0100

    Add a callBack function to mongoose connect

--- model/db.js
+++ model/db.js
@@ -1,2 +1,6 @@
+'use strict';
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/dbDoctor');
+mongoose.connect('mongodb://localhost/dbDoctor', function(err) {
+  if (err) throw err;
+  console.log('Successfully connected to MongoDB');
+});
